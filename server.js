a534f999c81129931f9d21f7768cf5434460e6c3
Gurdev Singh <gurdevs@Gurdevs-MacBook-Pro.local>
2020-06-19 16:31:55 +0530

    Added APIs to send switch signal ON/OFF data.

--- server.js
+++ server.js
@@ -5,10 +5,30 @@ const io = require('socket.io')(server);
 var ws;
 server.listen(8080);
 
-app.get('/', (req, res) => {
-  ws.emit('switch',{hello:'HELLLO'});
-  console.log('sending switch data');
-  res.send('Hello World!');
+app.get('/on', (req, res) => {
+  console.log('sending switch ON data');
+  if(ws != undefined) {
+    ws.emit('switch',{switch:'1'});
+    res.send('Switch ON signal sent.');
+  }
+  else {
+    console.log('websocket is unavailable, switch ON');
+    res.send('websocket is unavailable, switch ON');
+  }
+
+});
+
+app.get('/off', (req, res) => {
+  
+  console.log('sending switch OFF data');
+  if(ws != undefined) {
+    ws.emit('switch',{switch:'0'});
+    res.send('Switch OFF signal sent.');
+  }
+  else {
+    console.log('websocket is unavailable, switch OFF');
+    res.send('websocket is unavailable, switch OFF');
+  }
 });
 
 io.on('connection', (socket) => {
